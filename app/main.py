27da634f52c5079f5abd7b526ea390f6ebb179ee
Vova <vovakucin082@gmail.com>
2025-10-28 16:32:25 +0100

    corrections have been made

--- app/main.py
+++ app/main.py
@@ -1,20 +1,19 @@
 from typing import Callable
+from typing import Any
 from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
     completed_runs = {}
-
     @wraps(func)
-    def wrapper(*args):
+    def wrapper(*args) -> Any:
         key = (func.__name__, args)
         if key not in completed_runs:
             result = func(*args)
             completed_runs[key] = result
             print("Calculating new result")
             return result
-        else:
-            print("Getting from cache")
-            return completed_runs[key]
+        print("Getting from cache")
+        return completed_runs[key]
 
     return wrapper
